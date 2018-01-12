36d833b65ab89ba8b70c1c60676a50e831a1d845
estrane <zeldrew@hotmail.com>
2018-01-12 00:51:59 -0800

    Finished project

---
+++ app/data/friend.js
@@ -0,0 +1,54 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Harry Huffman',
+    profilePic: 'https://maximumwage.files.wordpress.com/2013/01/scarylookingguy.jpg',
+    scores: [
+      "1",
+      "5",
+      "4",
+      "3",
+      "4",
+      "5",
+      "4",
+      "2",
+      "3",
+      "3"
+    ]
+  },
+  {
+    name: 'Old Man Jenkins',
+    profilePic: 'https://c2.staticflickr.com/8/7175/6698567177_fc5df89f18_b.jpg',
+    scores: [
+      "3",
+      "5",
+      "2",
+      "2",
+      "1",
+      "1",
+      "4",
+      "4",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: 'Patricia Maverick',
+    profilePic: 'http://i.imgur.com/FsaErgQ.jpg',
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "1",
+      "2",
+      "1",
+      "5",
+      "5"
+    ]
+  }
+]
+
+module.exports = friendList;
